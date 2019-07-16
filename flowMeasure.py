e5d0fddb465407534456877d848eaca34af87c14
jmathet <jmathet@enseirb-matmeca.fr>
2019-07-16 14:14:35 +1000

    Computation number of switch in each layer OK

--- flowMeasure.py
+++ flowMeasure.py
@@ -49,18 +49,18 @@ def getPortStat(url):
     return portBits
 
 def getFlowStat(r):
-    # url : url on the API REST PATH for flow statistic
     # r : link rate paraneter (to set the desired average link utilisation)
 
-    listLEdge_up_p_e = {} # Number of links for edge swicth e in pod p (where 2*p+e is the list index)
-    listLAgg_down_p = {} # Number of down links needs to suport the down-traffic (where the pod number p is the list index)
-    NAgg_up_p = {} # TODO comment (p index)
-
-    for i in range(0,4):
-        rate = 0
-        for j in range(0,2):
-            url1 = FLOWSSTAT_URL + "?device=" + EDGE_DEVICES[2*i+j] + "&port=1"
-            url2 = FLOWSSTAT_URL + "?device=" + EDGE_DEVICES[2*i+j] + "&port=2"
+    k = 4 # Switch degree
+    listLAgg_down_p = [] # Number of down links required to suport the down-traffic (where the pod number p is the list index)
+    listLAgg_up_p = [] # Number of down links required to suport the up-traffic (where the pod number p is the list index)
+    NAgg_p = [] # Minimum number of active aggregation switches reauired to support traffic (where the pod number p is the list index)
+    
+    for p in range(0,4): # For each pod
+        listLEdge_up_p_e = [] # Number of links for edge swicth e in pod p (where 2*p+e is the list index)
+        for j in range(0,2): # For each edge switch in the pod p 
+            url1 = FLOWSSTAT_URL + "?device=" + EDGE_DEVICES[2*p+j] + "&port=1"
+            url2 = FLOWSSTAT_URL + "?device=" + EDGE_DEVICES[2*p+j] + "&port=2"
 
             flowStat1 = getJsonData(url1)
             flowStat2 = getJsonData(url2)
@@ -70,60 +70,57 @@ def getFlowStat(r):
             rate1_down = flowStat1["loads"][1]["rate"] # rate between edge j and a1 of the pod p in the down direction 
             rate2_down = flowStat2["loads"][1]["rate"] # rate between edge j and a2 of the pod p in the down direction
 
-            LEdge_up_p_e = (rate1_up + rate2_up)*8*10**(-9) # total rate up in Gbits/sec
-            LEdge_down_p_e = (rate1_down + rate2_down)*8*10**(-9) # total rate down in Gbits/sec
+            LEdge_up_p_e = math.ceil((rate1_up + rate2_up)*8*10**(-9)) # total rate up in Gbits/sec
+            LEdge_down_p_e = math.ceil((rate1_down + rate2_down)*8*10**(-9)) # total rate down in Gbits/sec
 
-            # listLEdge_up_p_e.append(LEdge_up_p_e) # ERREUR 
+            listLEdge_up_p_e.append(LEdge_up_p_e)
 
-            LEdge_p_e = max(math.ceil(LEdge_up_p_e/r),math.ceil(LEdge_down_p_e/r),1) # 1GBytes links
+            LEdge_p_e = max(LEdge_up_p_e/r,LEdge_down_p_e/r,1) # 1GBytes links
 
-            print("Number of links needs between the edge swicth " + str(2*i+j) + " and the aggregation layer")
+            print("Number of links needs between the edge swicth " + str(2*p+j) + " and the aggregation layer")
             print("LEdge_up_p_e = " + str(LEdge_up_p_e) + " Gbits/sec")
             print("LEdge_down_p_e = " + str(LEdge_down_p_e) + " Gbits/sec")
             print("LEdge_p_e = " + str(LEdge_p_e) + " (1 Gbits/sec links)")
         
-        # NAgg_up_p = NAgg_up_p.extend(max(listLEdge_up_p_e))
+        NAgg_up_p = max(listLEdge_up_p_e)
+        print("[POD " + str(p) + " ] " + "Minimum number of aggregation switches (to satisfy up traffic) " + str(NAgg_up_p))
 
-
-    print("**************************************************************************")
-    for i in range(0,4): # pod range
-        rate_up = 0
-        rate_down = 0
-        for j in range(0,2): # Aggr devices in the pod
-            url1 = FLOWSSTAT_URL + "?device=" + AGREGATION_DEVICES[2*i+j] + "&port=1"
-            url2 = FLOWSSTAT_URL + "?device=" + AGREGATION_DEVICES[2*i+j] + "&port=2"
+        
+        LAgg_up_p = 0
+        LAgg_down_p = 0
+        for j in range(0,2): # For each aggregation switch in the pod p 
+            url1 = FLOWSSTAT_URL + "?device=" + AGREGATION_DEVICES[2*p+j] + "&port=1"
+            url2 = FLOWSSTAT_URL + "?device=" + AGREGATION_DEVICES[2*p+j] + "&port=2"
 
             flowStat1 = getJsonData(url1)
             flowStat2 = getJsonData(url2)
 
-            rate1_up = flowStat1["loads"][0]["rate"] # rate between aggregation j and c1 of the pod p in the up direction
-            rate2_up = flowStat2["loads"][0]["rate"] # rate between edge j and c2 of the pod p in the up direction
-            rate1_down = flowStat1["loads"][1]["rate"] # rate between edge j and c1 of the pod p in the down direction 
-            rate2_down = flowStat2["loads"][1]["rate"] # rate between edge j and c2 of the pod p in the down direction
+            rate1_up = flowStat1["loads"][0]["rate"] # rate between aggregation switch j and c1 of the pod p in the up direction
+            rate2_up = flowStat2["loads"][0]["rate"] # rate between aggregation switch j and c2 of the pod p in the up direction
+            rate1_down = flowStat1["loads"][1]["rate"] # rate between aggregation switch j and c1 of the pod p in the down direction 
+            rate2_down = flowStat2["loads"][1]["rate"] # rate between aggregation switch j and c2 of the pod p in the down direction
 
-            LAgg_up_p = rate_up + (rate1_up + rate2_up)*8*10**(-9) # total rate up in Gbits/sec
-            LAgg_down_p = rate_down + (rate1_down + rate2_down)*8*10**(-9) # total rate down in Gbits/sec
+            LAgg_up_p = LAgg_up_p + (rate1_up + rate2_up)*8*10**(-9) # total rate up in Gbits/sec
+            LAgg_down_p = LAgg_down_p + (rate1_down + rate2_down)*8*10**(-9) # total rate down in Gbits/sec
 
-        # listLAgg_down_p.extend(LAgg_down_p) # ERREUR
-
+        listLAgg_up_p.append(LAgg_up_p)
         LAgg_p = max(math.ceil(LAgg_up_p/r),math.ceil(LAgg_down_p/r),1) # 1GBytes links
+       
+        NAgg_down_p = math.ceil(LAgg_down_p/(k/2))
+        NAgg_p.append(max(NAgg_up_p,NAgg_down_p,1))
 
-        k = 4 # Switch degree
-        # NAgg_down_p = math.ceil(max(listLAgg_down_p/k/2)) # ERREUR SANS DOUTE
-
-        # NAgg_p = max(NAgg_up_p(i),) # A FINIR
-
-
-        print("Number of links needs between aggregation swicthes of the pod " + str(i) + " and the core layer")
+        print("Number of links needs between aggregation swicthes of the pod " + str(p) + " and the core layer")
         print("LAgg_up_p = " + str(LAgg_up_p) + " Gbits/sec")
         print("LAgg_down_p = " + str(LAgg_down_p) + " Gbits/sec")
         print("LAgg_p = " + str(LAgg_p) + " (1 Gbits/sec links)")
-        
-
-    print("**************************************************************************")
-
+        print("******************************NEXT POD********************************************")
     
-    return flowStat1
+    NCore = math.ceil(max(NAgg_p)) # Minimum of core switches to satisfy the demand
+
+    print("NAgg_p = " + str(NAgg_p))
+    print("Ncore = " + str(NCore))
+
+    return [NCore, NAgg_p]
 
 def portNumber2Mac(halfurl, deviceId):
     deviceId = deviceId.replace(":", "%3A")
@@ -161,7 +158,7 @@ def send(url, chains, sliceInfo):
     data = {}
 
     portSpeed = getportSpeed(url)
-
+#print(json.dumps(flowStat1, indent=4, sort_keys=True))
     for chainName in chains.keys():
         data["CHAIN_NAME"].append(chainName)
         chainSpeed ={}
@@ -181,6 +178,7 @@ def send(url, chains, sliceInfo):
 
 if __name__ == "__main__":
     getFlowStat(1)
+    #print(json.dumps(flowStat1, indent=4, sort_keys=True))
     '''
     boolean flag = False
     delta = 0
