3de89b3ac96e2420636f41f2e07b6965ea218acc
jmathet <jmathet@enseirb-matmeca.fr>
2019-07-25 10:53:43 +1000

    Add Mininet topology generation

---
+++ mininet/fattree_k4.py
@@ -0,0 +1,116 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Node
+from mininet.node import Controller, RemoteController
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+from mininet.link import TCLink
+
+
+class FatTree( Topo ):
+
+    CoreSwitchList = []
+    AggSwitchList = []
+    EdgeSwitchList = []
+    HostList = []
+
+    def __init__( self, k):
+        " Create Fat Tree topo."
+        self.pod = k
+        self.iCoreLayerSwitch = (k/2)**2
+        self.iAggLayerSwitch = k*k/2
+        self.iEdgeLayerSwitch = k*k/2
+        self.density = k/2
+        self.iHost = self.iEdgeLayerSwitch * self.density
+
+
+
+        # Init Topo
+        Topo.__init__(self)
+
+        self.createTopo()
+
+        self.createLink(1000)
+
+    def createTopo(self):
+        self.createCoreLayerSwitch(self.iCoreLayerSwitch)
+        self.createAggLayerSwitch(self.iAggLayerSwitch)
+        self.createEdgeLayerSwitch(self.iEdgeLayerSwitch)
+        self.createHost(self.iHost)
+
+    """
+    Create Switch and Host
+    """
+
+    def _addSwitch(self, number, level, switch_list):
+        for x in xrange(1, number+1):
+            PREFIX = str(level) + "00"
+            if x >= int(10):
+                PREFIX = str(level) + "0"
+            switch_list.append(self.addSwitch('s' + PREFIX + str(x)))
+
+    def createCoreLayerSwitch(self, NUMBER):
+        self._addSwitch(NUMBER, 1, self.CoreSwitchList)
+
+    def createAggLayerSwitch(self, NUMBER):
+        self._addSwitch(NUMBER, 2, self.AggSwitchList)
+
+    def createEdgeLayerSwitch(self, NUMBER):
+        self._addSwitch(NUMBER, 3, self.EdgeSwitchList)
+
+    def createHost(self, NUMBER):
+        for x in xrange(1, NUMBER+1):
+            PREFIX = "h"
+            self.HostList.append(self.addHost(PREFIX + str(x)))
+
+    """
+    Add Link
+    """
+    def createLink(self, bandwidth):
+        # bandwidth in Mbit
+        linkopts = dict(bw=bandwidth) 
+        end = self.pod/2
+        for x in xrange(0, self.iAggLayerSwitch, end):
+            for i in xrange(0, end):
+                for j in xrange(0, end):
+                    self.addLink(
+                        self.CoreSwitchList[i*end+j],
+                        self.AggSwitchList[x+i],
+                        **linkopts)
+
+        for x in xrange(0, self.iAggLayerSwitch, end):
+            for i in xrange(0, end):
+                for j in xrange(0, end):
+                    self.addLink(
+                        self.AggSwitchList[x+i],
+                        self.EdgeSwitchList[x+j],
+                        **linkopts)
+
+        for x in xrange(0, self.iEdgeLayerSwitch):
+            for i in xrange(0, self.density):
+                self.addLink(
+                    self.EdgeSwitchList[x],
+                    self.HostList[self.density * x + i],
+                    **linkopts)
+
+
+topos = { 'fattree' : ( lambda k : FatTree(k)) }
+
+
+def runMyNetwork():
+    "Create Fat Tree network"
+    mytopo = FatTree(4)
+    net = Mininet(topo=mytopo, link = TCLink, controller=RemoteController( 'c0', ip='130.194.73.219')) #TODO : mettre le bon controleur
+    #net = Mininet(topo=mytopo, link=TCLink)
+    net.start()
+    CLI(net)
+
+    #net.pingAll()
+    net.stop()
+
+
+
+if __name__ == '__main__':
+    # Tell mininet to print useful information
+    setLogLevel('info')
+    runMyNetwork()
